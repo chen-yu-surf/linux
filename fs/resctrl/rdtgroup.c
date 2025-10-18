1562e6a24d3e13826dd212bc36f146e84122394b
Chen Yu <yu.c.chen@intel.com>
2025-10-18 21:18:01 +0800

    x86/resctrl: Create per region schemata for the resource of RDT_RESOURCE_MBA_REGIONx
    Each region-aware RDT resource with a rid of RDT_RESOURCE_MBA_REGIONx
    has up to 4 regions. Create at most 4 schemata for these 4 regions. A
    typical example would be:
    
    MB_LOCAL:1=100,2=100
    MB_REMOTE:1=100,2=100
    MB_TIER2_LOCAL:1=80,2=30
    MB_TIER2_REMOTE:1=80,2=30
    MB_TIER3_LOCAL:1=80,2=30
    MB_TIER3_REMOTE:1=80,2=30
    MB_TIER4_LOCAL:1=80,2=30
    MB_TIER4_REMOTE:1=80,2=30
    
    Each line represents the configuration for one region. Users can
    customize the MBA settings for each line individually.
    
    TBD: Currently, the percentage value corresponds to the Optimal
    MBA setting. Support for minimum (min) and maximum (max) MBA settings
    will be added in the formal version. There is a discussion at
    https://lore.kernel.org/all/aPtfMFfLV1l%2FRB0L@e133380.arm.com/
    
    Suggested-by: Tony Luck <tony.luck@intel.com>
    Suggested-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -2622,7 +2622,8 @@ out_done:
 	return ret;
 }
 
-static int schemata_list_add(struct rdt_resource *r, enum resctrl_conf_type type)
+static int schemata_list_add(struct rdt_resource *r, enum resctrl_conf_type type,
+			     int region)
 {
 	struct resctrl_schema *s;
 	const char *suffix = "";
@@ -2650,7 +2651,17 @@ static int schemata_list_add(struct rdt_resource *r, enum resctrl_conf_type type
 		break;
 	}
 
-	ret = snprintf(s->name, sizeof(s->name), "%s%s", r->name, suffix);
+	if (region != -1) {
+		char *display_name = get_mrrm_region_name(region, true);
+
+		if (!display_name)
+			return -EINVAL;
+
+		ret = snprintf(s->name, sizeof(s->name), "%s_%s",
+			       r->name, display_name);
+	} else {
+		ret = snprintf(s->name, sizeof(s->name), "%s%s", r->name, suffix);
+	}
 	if (ret >= sizeof(s->name)) {
 		kfree(s);
 		return -EINVAL;
@@ -2696,13 +2707,17 @@ static int schemata_list_create(void)
 
 	for_each_alloc_capable_rdt_resource(r) {
 		if (resctrl_arch_get_cdp_enabled(r->rid)) {
-			ret = schemata_list_add(r, CDP_CODE);
+			ret = schemata_list_add(r, CDP_CODE, -1);
 			if (ret)
 				break;
 
-			ret = schemata_list_add(r, CDP_DATA);
+			ret = schemata_list_add(r, CDP_DATA, -1);
+		} else if (RESOURCE_IS_MBA_REGION(r->rid)) {
+			/* the region might be unavailable, ignore it */
+			schemata_list_add(r, CDP_NONE,
+					  MBA_REGION_IDX(r->rid));
 		} else {
-			ret = schemata_list_add(r, CDP_NONE);
+			ret = schemata_list_add(r, CDP_NONE, -1);
 		}
 
 		if (ret)
