4a0726c15c4ae784671240ce0fb01aa069d7ca49
Chen Yu <yu.c.chen@intel.com>
2025-10-19 21:26:24 +0800

    x86/resctrl: Support region based MBA schemata display
    Each domain has up to 4 regions. Their memory bandwidth will be
    displayed accordingly. The typical format is as follows:
    
      MB_LOCAL:1=100,2=100
      MB_REMOTE:1=100,2=100
      MB_TIER2_LOCAL:1=80,2=30
      MB_TIER2_REMOTE:1=80,2=30
      MB_TIER3_LOCAL:1=80,2=30
      MB_TIER3_REMOTE:1=80,2=30
      MB_TIER4_LOCAL:1=80,2=30
      MB_TIER4_REMOTE:1=80,2=30
    
    Take MB_TIER2_LOCAL:1=80,2=30 for example,
    it means the local memory bandwidth for memory tiering 2 on
    domain1 is 80%, on domain2 is 30%.
    
    Suggested-by: Tony Luck <tony.luck@intel.com>
    Suggested-by: Reinette Chatre <reinette.chatre@intel.com>
    Co-developed-by: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Signed-off-by: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>

--- fs/resctrl/ctrlmondata.c
+++ fs/resctrl/ctrlmondata.c
@@ -399,15 +399,19 @@ static void show_doms(struct seq_file *s, struct resctrl_schema *schema, int clo
 	lockdep_assert_cpus_held();
 
 	seq_printf(s, "%*s:", max_name_width, schema->name);
+
 	list_for_each_entry(dom, &r->ctrl_domains, hdr.list) {
 		if (sep)
 			seq_puts(s, ";");
 
-		if (is_mba_sc(r))
+		if (is_mba_sc(r)) {
 			ctrl_val = dom->mbps_val[closid];
-		else
+		} else if (RESOURCE_IS_MBA_REGION(r->rid)) {
+			ctrl_val = resctrl_arch_get_region_config(r, dom, closid);
+		} else {
 			ctrl_val = resctrl_arch_get_config(r, dom, closid,
 							   schema->conf_type);
+		}
 
 		seq_printf(s, schema->fmt_str, dom->hdr.id, ctrl_val);
 		sep = true;
