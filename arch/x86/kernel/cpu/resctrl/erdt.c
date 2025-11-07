027dab28bc9cbaa76cf040f43ca6255bf39c93fd
Chen Yu <yu.c.chen@intel.com>
2025-11-07 22:57:16 +0800

    x86/resctrl: Introduce region based MBA write implementation
    Introduce the function to do region based MBA MMIO space
    write for later use.
    
    The input parameters are the CLOS id and the region number,
    the corresponding mmio address is manipulated.
    
    Co-developed-by: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Signed-off-by: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>

--- arch/x86/kernel/cpu/resctrl/erdt.c
+++ arch/x86/kernel/cpu/resctrl/erdt.c
@@ -104,6 +104,30 @@ static u64 apply_correction_factor(u64 val, u32 factor)
 	return ((val * factor) >> FIXPOINT_LOW_BITS);
 }
 
+void erdt_ctrl_update(int domid, u32 ctrl_val, int closid, int region)
+{
+	struct acpi_erdt_marc *marc = NULL;
+	struct erdt_domain_info *d;
+	void __iomem *vaddr;
+
+	d = xa_load(&erdt_domain_xa, domid);
+	if (!d)
+		return;
+
+	marc = d->marc;
+	if (!marc)
+		return;
+
+	/*
+	 * Write to optimal memory bandwidth for now.
+	 * TBD: minimum/maximum memory bandwidth.
+	 * MMIO_ADDRESS_for_CLOS# = MBA Optimal BW Register Block Base
+	 * Address + Floor(Region# / 4) x 512B + CLOS# x 8B
+	 */
+	vaddr = d->base[ERDT_MMIO_MARC_OPT] + (region / 4) * 512 + closid * 8;
+	writeq(ctrl_val, vaddr);
+}
+
 static u64 erdt_read_region_mbm(struct rdt_domain_hdr *hdr,
 				struct erdt_domain_info *d, int rmid,
 				int eventid)
