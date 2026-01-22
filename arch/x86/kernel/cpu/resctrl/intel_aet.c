a1cd7e02f0a26b14b9e5beca2299250ea6f2ef1d
Tony Luck <tony.luck@intel.com>
2026-01-22 12:03:51 -0800

    x86/resctrl: Add energy event support for Diamond Rapids
    Parameters for telemetry events may change for each CPU generation.
    
    Diamond Rapids supports fewer RMIDs than Clearwater Forest, but
    otherwise support for energy telemetry is the same.
    
    Add a new struct event_group for the GUID used by Diamond Rapids
    for energy telemetry.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/x86/kernel/cpu/resctrl/intel_aet.c
+++ arch/x86/kernel/cpu/resctrl/intel_aet.c
@@ -125,8 +125,24 @@ static struct event_group perf_0x26557651 = {
 	}
 };
 
+/*
+ * Link: Not published yet
+ */
+static struct event_group energy_0x48543760 = {
+	.pfname		= "energy",
+	.guid		= 0x48543760,
+	.num_rmid	= 256,
+	.mmio_size	= XML_MMIO_SIZE(256, 2, 3),
+	.num_events	= 2,
+	.evts		= {
+		EVT(PMT_EVENT_ENERGY, 0, 18),
+		EVT(PMT_EVENT_ACTIVITY, 1, 18),
+	}
+};
+
 static struct event_group *known_event_groups[] = {
 	&energy_0x26696143,
+	&energy_0x48543760,
 	&perf_0x26557651,
 };
 
