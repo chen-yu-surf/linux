9a4572ff2318dca558db9257176462513befc2a3
Chen Yu <yu.c.chen@intel.com>
2026-03-06 14:04:52 +0800

    x86/resctrl: Add fake MRRM ERDT table for region aware test on VM
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>

--- arch/x86/kernel/cpu/resctrl/monitor.c
+++ arch/x86/kernel/cpu/resctrl/monitor.c
@@ -457,7 +457,7 @@ int __init rdt_get_l3_mon_config(struct rdt_resource *r)
 
 	resctrl_rmid_realloc_limit = boot_cpu_data.x86_cache_size * 1024;
 	hw_res->mon_scale = boot_cpu_data.x86_cache_occ_scale / snc_nodes_per_l3_cache;
-	r->mon.num_rmid = (boot_cpu_data.x86_cache_max_rmid + 1) / snc_nodes_per_l3_cache;
+	r->mon.num_rmid = 16;
 	hw_res->mbm_width = MBM_CNTR_WIDTH_BASE;
 
 	if (mbm_offset > 0 && mbm_offset <= MBM_CNTR_WIDTH_OFFSET_MAX)
