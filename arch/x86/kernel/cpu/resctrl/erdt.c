961d0eca263d615db30fa349406af45b6ba107cc
Chen Yu <yu.c.chen@intel.com>
2026-03-12 18:45:21 +0800

    x86/resctrl: Parse ACPI MMRC table
    The MMRC table is used to provide Memory Bandwidth Monitor Register
    information. Its register addresses are mapped, and the MMRC pointer
    is saved in the corresponding domain information entry.
    
    Suggested-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>

--- arch/x86/kernel/cpu/resctrl/erdt.c
+++ arch/x86/kernel/cpu/resctrl/erdt.c
@@ -24,6 +24,7 @@ static bool erdt_enabled;
 
 #define ERDT_VALID_VERSION		1
 #define CMRC_SUPPORTED_INDEX_FN		1
+#define MMRC_SUPPORTED_INDEX_FN		1
 #define RMDD_FLAG_CPU_L3_DOMAIN		BIT(0)
 
 /* Set in a monitoring counter when it holds no valid data to report. */
@@ -141,6 +142,7 @@ static void cleanup_one_domain(struct erdt_domain_info *d)
 {
 	erdt_iounmap_domain(d);
 	kfree(d->cmrc);
+	kfree(d->mmrc);
 	kfree(d);
 }
 
@@ -207,6 +209,36 @@ static __init int cmrc_init(struct acpi_subtbl_hdr_16 *subtbl,
 	}
 
 	erdt_scale = max_t(int, erdt_scale, cmrc->up_scale);
+
+	return 0;
+}
+
+static __init int mmrc_init(struct acpi_subtbl_hdr_16 *subtbl,
+			    struct erdt_domain_info *domain_info)
+{
+	struct acpi_erdt_mmrc *mmrc = (struct acpi_erdt_mmrc *)subtbl;
+
+	if (subtbl->length < sizeof(*mmrc)) {
+		pr_warn(FW_BUG "Truncated MMRC subtable\n");
+		return -EIO;
+	}
+
+	if (mmrc->index_fn != MMRC_SUPPORTED_INDEX_FN) {
+		pr_info("Unsupported MMRC index function %d\n", mmrc->index_fn);
+		return -EIO;
+	}
+
+	domain_info->base[ERDT_MMIO_MMRC_BASE] =
+		erdt_ioremap(mmrc->reg_base, mmrc->reg_size, "MMRC base");
+	if (!domain_info->base[ERDT_MMIO_MMRC_BASE])
+		return -EIO;
+
+	domain_info->mmrc = kmemdup(mmrc, subtbl->length, GFP_KERNEL);
+	if (!domain_info->mmrc) {
+		iounmap(domain_info->base[ERDT_MMIO_MMRC_BASE]);
+		domain_info->base[ERDT_MMIO_MMRC_BASE] = NULL;
+		return -ENOMEM;
+	}
 
 	return 0;
 }
@@ -286,6 +318,12 @@ static __init bool parse_rmdd_table(struct acpi_subtbl_hdr_16 *rmdd_hdr)
 			if (!(subtbl_mask & BIT(ACPI_ERDT_TYPE_CMRC)) &&
 			    !cmrc_init(subtbl, domain_info))
 				subtbl_mask |= BIT(ACPI_ERDT_TYPE_CMRC);
+
+			break;
+		case ACPI_ERDT_TYPE_MMRC:
+			if (!(subtbl_mask & BIT(ACPI_ERDT_TYPE_MMRC)) &&
+			    !mmrc_init(subtbl, domain_info))
+				subtbl_mask |= BIT(ACPI_ERDT_TYPE_MMRC);
 
 			break;
 		default:
--- arch/x86/kernel/cpu/resctrl/internal.h
+++ arch/x86/kernel/cpu/resctrl/internal.h
@@ -25,11 +25,13 @@
  * Index into erdt_domain_info::base[] for each MMIO region.
  * @ERDT_MMIO_RMDD_CREG: RMDD control register base address
  * @ERDT_MMIO_CMRC_BASE: CMRC monitoring register base address
+ * @ERDT_MMIO_MMRC_BASE: MMRC monitoring register base address
  */
 enum erdt_mmio_type {
 	ERDT_MMIO_RMDD_CREG,
 	ERDT_MMIO_CMRC_BASE,
-	ERDT_MMIO_LAST = ERDT_MMIO_CMRC_BASE
+	ERDT_MMIO_MMRC_BASE,
+	ERDT_MMIO_LAST = ERDT_MMIO_MMRC_BASE
 };
 
 #define ERDT_MMIO_NUM_TYPES	(ERDT_MMIO_LAST + 1)
@@ -38,6 +40,7 @@ enum erdt_mmio_type {
  * struct erdt_domain_info - Per-domain ERDT information
  * @base:	Array of ioremapped MMIO region base addresses, indexed by ERDT_MMIO_* type
  * @cmrc:	Copy of the ACPI CMRC sub-table for this domain
+ * @mmrc:	Copy of the ACPI MMRC sub-table for this domain
  * @cpu_mask:	CPUs belonging to this resource management domain
  * @max_rmid:	Maximum RMID supported by this domain
  * @dom_id:	L3 cache ID shared by all CPUs in this domain (-1 if unset)
@@ -46,6 +49,7 @@ enum erdt_mmio_type {
 struct erdt_domain_info {
 	void __iomem		*base[ERDT_MMIO_NUM_TYPES];
 	struct acpi_erdt_cmrc	*cmrc;
+	struct acpi_erdt_mmrc	*mmrc;
 	struct cpumask		cpu_mask;
 	u32			max_rmid;
 	int			dom_id;
