0f650a9978c811e7fe2bf84753334dd87b6ff94b
Reinette Chatre <reinette.chatre@intel.com>
2026-04-09 10:07:54 -0700

    fs/resctrl: Centralize private control properties
    Some properties of a control should never be changed by the architecture,
    for example how the control value is displayed to user space and how a
    control value provided by user space is interpreted. The latter is part
    of resctrl filesystem interface.
    
    Information on how to manage control values is scattered within resctrl.
    The parser to use is initialized during parsing while the display format
    is set as a final resource property. In preparation for multiple controls
    associated with a resource the display format can no longer be a property
    of the resource self.
    
    Introduce struct resctrl_ctrl_priv that contains control properties
    private to resctrl fs. Initialize the values for all supported controls
    and use the new data directly instead of needing to discover it
    every time when parsing user data.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    ---
    FIXME: To ensure safe array access, need to ensure that when new controls
    are created that they always have a valid type.

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -2762,20 +2762,6 @@ static int final_resources_list_add(struct rdt_resource *r, enum resctrl_conf_ty
 	if (cl > max_name_width)
 		max_name_width = cl;
 
-	switch (r->ctrl_type) {
-	case RESCTRL_CTRL_BITMAP:
-		f->fmt_str = "%d=%x";
-		break;
-	case RESCTRL_CTRL_SCALAR:
-		f->fmt_str = "%d=%u";
-		break;
-	}
-
-	if (WARN_ON_ONCE(!f->fmt_str)) {
-		kfree(f);
-		return -EINVAL;
-	}
-
 	INIT_LIST_HEAD(&f->list);
 	list_add(&f->list, &rdt_resource_final_all);
 
