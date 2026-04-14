2941d1fd093bb491f83efb27d0d5c8bc267a47a3
Reinette Chatre <reinette.chatre@intel.com>
2026-04-14 10:53:00 -0700

    x86/resctrl: Centralize cache control initialization
    The x86 cache control initialization is fragmented between setting of
    defaults and initialization. This is done inconsistently since some
    systems have hardcoded default values that should be discovered from
    other systems.
    
    Centralize cache control property initialization to make it clear
    how certain properties are initialization and simplify future support for
    multiple controls.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- arch/x86/kernel/cpu/resctrl/core.c
+++ arch/x86/kernel/cpu/resctrl/core.c
@@ -265,8 +265,17 @@ static void rdt_get_cache_alloc_cfg(int idx, struct rdt_resource *r)
 	r->ctrl.cache.cbm_len = eax.split.cbm_len + 1;
 	default_ctrl = BIT_MASK(eax.split.cbm_len + 1) - 1;
 	r->ctrl.cache.shareable_bits = ebx & default_ctrl;
-	if (boot_cpu_data.x86_vendor == X86_VENDOR_INTEL)
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_INTEL) {
 		r->ctrl.cache.arch_has_sparse_bitmasks = ecx.split.noncont;
+		r->ctrl.cache.min_cbm_bits = 1;
+	} else if (boot_cpu_data.x86_vendor == X86_VENDOR_AMD ||
+		   boot_cpu_data.x86_vendor == X86_VENDOR_HYGON) {
+		r->ctrl.cache.arch_has_sparse_bitmasks = true;
+		r->ctrl.cache.min_cbm_bits = 0;
+	} else {
+		return;
+	}
+
 	r->alloc_capable = true;
 }
 
@@ -1017,7 +1026,6 @@ static __init void rdt_init_res_defs_intel(void)
 		if (r->rid == RDT_RESOURCE_L3 ||
 		    r->rid == RDT_RESOURCE_L2) {
 			hw_res->has_per_cpu_cache_cfg = false;
-			r->ctrl.cache.min_cbm_bits = 1;
 		} else if (r->rid == RDT_RESOURCE_MBA) {
 			hw_res->msr_base = MSR_IA32_MBA_THRTL_BASE;
 			hw_res->msr_update = mba_wrmsr_intel;
@@ -1036,8 +1044,6 @@ static __init void rdt_init_res_defs_amd(void)
 		if (r->rid == RDT_RESOURCE_L3 ||
 		    r->rid == RDT_RESOURCE_L2) {
 			hw_res->has_per_cpu_cache_cfg = true;
-			r->ctrl.cache.arch_has_sparse_bitmasks = true;
-			r->ctrl.cache.min_cbm_bits = 0;
 		} else if (r->rid == RDT_RESOURCE_MBA) {
 			hw_res->msr_base = MSR_IA32_MBA_BW_BASE;
 			hw_res->msr_update = mba_wrmsr_amd;
