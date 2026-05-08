bb25d35c7b44ff8ae2f15edde978df2307352ec6
Reinette Chatre <reinette.chatre@intel.com>
2026-05-08 15:42:27 -0700

    arm,x86,fs/resctrl: Rename schema format as control format
    The schema format enum describes how a control value should be formatted
    when displayed to user space and how a control value provided by user space
    should be parsed.
    
    The schema format is created from the control type. Use accurate naming
    to make this clear:
                enum resctrl_schema_fmt -> enum resctrl_ctrl_type
                RESCTRL_SCHEMA_BITMAP -> RESCTRL_CTRL_BITMAP
                RESCTRL_SCHEMA_RANGE -> RESCTRL_CTRL_SCALAR
                rdt_resource::schema_fmt -> rdt_resource::ctrl_type
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    ---
    Changes since RFC v1:
    - New subject prefix.

--- drivers/resctrl/mpam_resctrl.c
+++ drivers/resctrl/mpam_resctrl.c
@@ -1000,7 +1000,7 @@ static int mpam_resctrl_control_init(struct mpam_resctrl_res *res)
 	switch (r->rid) {
 	case RDT_RESOURCE_L2:
 	case RDT_RESOURCE_L3:
-		r->schema_fmt = RESCTRL_SCHEMA_BITMAP;
+		r->ctrl_type = RESCTRL_CTRL_BITMAP;
 		r->cache.arch_has_sparse_bitmasks = true;
 
 		r->cache.cbm_len = class->props.cpbm_wd;
@@ -1027,7 +1027,7 @@ static int mpam_resctrl_control_init(struct mpam_resctrl_res *res)
 		r->alloc_capable = true;
 		break;
 	case RDT_RESOURCE_MBA:
-		r->schema_fmt = RESCTRL_SCHEMA_RANGE;
+		r->ctrl_type = RESCTRL_CTRL_SCALAR;
 		r->ctrl_scope = RESCTRL_L3_CACHE;
 		r->bw_throttle_mode = THREAD_THROTTLE_UNDEFINED;
 
