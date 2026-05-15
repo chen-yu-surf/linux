5e5e77931839f37a44cd2f032342ff0ac4d59f86
Reinette Chatre <reinette.chatre@intel.com>
2026-05-15 14:54:18 -0700

    fs/resctrl: Clear staged configs of all controls
    Every control stages new control values in each of its domains'
    rdt_ctrl_domain::staged_config[]. With support for multiple controls
    this staged config needs to be cleared for all controls belonging to the
    resource.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -113,13 +113,16 @@ void rdt_last_cmd_printf(const char *fmt, ...)
 void rdt_staged_configs_clear(void)
 {
 	struct rdt_ctrl_domain *dom;
+	struct resctrl_ctrl *ctrl;
 	struct rdt_resource *r;
 
 	lockdep_assert_held(&rdtgroup_mutex);
 
 	for_each_alloc_capable_rdt_resource(r) {
-		list_for_each_entry(dom, &r->ctrl.domains, hdr.list)
-			memset(dom->staged_config, 0, sizeof(dom->staged_config));
+		for_each_resource_ctrl(ctrl, r) {
+			list_for_each_entry(dom, &ctrl->domains, hdr.list)
+				memset(dom->staged_config, 0, sizeof(dom->staged_config));
+		}
 	}
 }
 
