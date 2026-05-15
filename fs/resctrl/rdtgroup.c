b47ddea6cbea2fd347b6a87f1350353d196e76dd
Reinette Chatre <reinette.chatre@intel.com>
2026-05-15 16:12:33 -0700

    mpam,x86,fs/resctrl: Check for online domains per control
    There could be multiple controls each with its own list of domains.
    
    Expand the check for online domains to check the domain list of every
    control.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -4741,6 +4741,7 @@ cleanup_mountpoint:
 
 static bool resctrl_online_domains_exist(void)
 {
+	struct resctrl_ctrl *ctrl;
 	struct rdt_resource *r;
 
 	/*
@@ -4748,8 +4749,10 @@ static bool resctrl_online_domains_exist(void)
 	 * to return dummy 'not capable' resources.
 	 */
 	for_each_alloc_capable_rdt_resource(r) {
-		if (!list_empty(&r->ctrl.domains))
-			return true;
+		for_each_resource_ctrl(ctrl, r) {
+			if (!list_empty(&ctrl->domains))
+				return true;
+		}
 	}
 
 	for_each_mon_capable_rdt_resource(r) {
