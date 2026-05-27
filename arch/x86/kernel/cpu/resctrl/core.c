46ffdf5b312017b42040be37b31ec7e3ad4e0277
Reinette Chatre <reinette.chatre@intel.com>
2026-05-26 21:48:28 -0700

    x86/resctrl: Prepare to add domains to multiple controls
    Every control contains the list of domains controlled by it. Provide the
    control as parameter to domain add/remove so that when a CPU comes and
    goes its domain associated with every control is appropriately managed.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- arch/x86/kernel/cpu/resctrl/core.c
+++ arch/x86/kernel/cpu/resctrl/core.c
@@ -464,10 +464,11 @@ static int get_domain_id_from_scope(int cpu, enum resctrl_scope scope)
 	return -EINVAL;
 }
 
-static void domain_add_cpu_ctrl(int cpu, struct rdt_resource *r)
+static void domain_add_cpu_ctrl(int cpu, struct rdt_resource *r,
+				struct resctrl_ctrl *ctrl)
 {
 	struct rdt_hw_resource *hw_res = resctrl_to_arch_res(r);
-	int id = get_domain_id_from_scope(cpu, r->ctrl.scope);
+	int id = get_domain_id_from_scope(cpu, ctrl->scope);
 	struct rdt_hw_ctrl_domain *hw_dom;
 	struct list_head *add_pos = NULL;
 	struct rdt_domain_hdr *hdr;
@@ -478,11 +479,11 @@ static void domain_add_cpu_ctrl(int cpu, struct rdt_resource *r)
 
 	if (id < 0) {
 		pr_warn_once("Can't find control domain id for CPU:%d scope:%d for resource %s\n",
-			     cpu, r->ctrl.scope, r->name);
+			     cpu, ctrl->scope, r->name);
 		return;
 	}
 
-	hdr = resctrl_find_domain(&r->ctrl.domains, id, &add_pos);
+	hdr = resctrl_find_domain(&ctrl->domains, id, &add_pos);
 	if (hdr) {
 		if (!domain_header_is_valid(hdr, RESCTRL_CTRL_DOMAIN, r->rid))
 			return;
@@ -600,15 +601,20 @@ static void domain_add_cpu_mon(int cpu, struct rdt_resource *r)
 
 static void domain_add_cpu(int cpu, struct rdt_resource *r)
 {
-	if (r->alloc_capable)
-		domain_add_cpu_ctrl(cpu, r);
+	struct resctrl_ctrl *ctrl;
+
+	if (r->alloc_capable) {
+		for_each_resource_ctrl(ctrl, r)
+			domain_add_cpu_ctrl(cpu, r, ctrl);
+	}
 	if (r->mon_capable)
 		domain_add_cpu_mon(cpu, r);
 }
 
-static void domain_remove_cpu_ctrl(int cpu, struct rdt_resource *r)
+static void domain_remove_cpu_ctrl(int cpu, struct rdt_resource *r,
+				   struct resctrl_ctrl *ctrl)
 {
-	int id = get_domain_id_from_scope(cpu, r->ctrl.scope);
+	int id = get_domain_id_from_scope(cpu, ctrl->scope);
 	struct rdt_hw_ctrl_domain *hw_dom;
 	struct rdt_domain_hdr *hdr;
 	struct rdt_ctrl_domain *d;
@@ -617,11 +623,11 @@ static void domain_remove_cpu_ctrl(int cpu, struct rdt_resource *r)
 
 	if (id < 0) {
 		pr_warn_once("Can't find control domain id for CPU:%d scope:%d for resource %s\n",
-			     cpu, r->ctrl.scope, r->name);
+			     cpu, ctrl->scope, r->name);
 		return;
 	}
 
-	hdr = resctrl_find_domain(&r->ctrl.domains, id, NULL);
+	hdr = resctrl_find_domain(&ctrl->domains, id, NULL);
 	if (!hdr) {
 		pr_warn("Can't find control domain for id=%d for CPU %d for resource %s\n",
 			id, cpu, r->name);
@@ -712,8 +718,12 @@ static void domain_remove_cpu_mon(int cpu, struct rdt_resource *r)
 
 static void domain_remove_cpu(int cpu, struct rdt_resource *r)
 {
-	if (r->alloc_capable)
-		domain_remove_cpu_ctrl(cpu, r);
+	struct resctrl_ctrl *ctrl;
+
+	if (r->alloc_capable) {
+		for_each_resource_ctrl(ctrl, r)
+			domain_remove_cpu_ctrl(cpu, r, ctrl);
+	}
 	if (r->mon_capable)
 		domain_remove_cpu_mon(cpu, r);
 }
