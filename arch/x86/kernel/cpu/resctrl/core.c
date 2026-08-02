370d2a9b20a18e96dad3a8e47ea4732ac126d591
Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
2026-08-03 00:02:47 +0800

    x86/resctrl: Parse ACPI ERDT table and save CACD cpumask for RMDD domains
    Parse the RMDD subtables within the ERDT ACPI table and their nested
    CACD entries to construct per-domain CPU masks.
    
    There is one ERDT table per platform. Each RMDD describes one resource
    management domain (RMD), also known as an L3 domain, and carries MMIO
    base information for later monitoring support.
    
    For each RMDD, parse the associated CACD, map its x2APIC IDs to logical
    CPUs, and save the resulting CPU mask. This mask associates each ERDT
    domain with the CPUs that belong to it and is used later when attaching
    ERDT data to resctrl monitoring domains.
    
    Suggested-by: Tony Luck <tony.luck@intel.com>
    Suggested-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>
    Tested-by: Hongyu Ning <hongyu.ning@linux.intel.com>

--- arch/x86/kernel/cpu/resctrl/core.c
+++ arch/x86/kernel/cpu/resctrl/core.c
@@ -1300,6 +1300,7 @@ static __init void check_quirks(void)
 
 static __init bool get_rdt_resources(void)
 {
+	erdt_init();
 	rdt_alloc_capable = get_rdt_alloc_resources();
 	rdt_mon_capable = get_rdt_mon_resources();
 
@@ -1385,7 +1386,7 @@ void resctrl_cpu_detect(struct cpuinfo_x86 *c)
 	}
 }
 
-static int __init resctrl_arch_late_init(void)
+static int __init __resctrl_arch_late_init(void)
 {
 	struct rdt_resource *r;
 	int state, ret, i;
@@ -1428,6 +1429,15 @@ static int __init resctrl_arch_late_init(void)
 	return 0;
 }
 
+static int __init resctrl_arch_late_init(void)
+{
+	int ret = __resctrl_arch_late_init();
+
+	if (ret)
+		erdt_exit();
+	return ret;
+}
+
 late_initcall(resctrl_arch_late_init);
 
 static void __exit resctrl_arch_exit(void)
@@ -1441,6 +1451,8 @@ static void __exit resctrl_arch_exit(void)
 	 * removed here.
 	 */
 	resctrl_exit();
+
+	erdt_exit();
 }
 
 __exitcall(resctrl_arch_exit);
